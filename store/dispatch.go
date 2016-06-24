c7cde0a0ef949e1d679daceb12703a88a527185e
Shaun Crampton <shaun@tigera.io>
2016-06-24 15:15:24 +0100

    Add an event dispatcher.

--- store/dispatch.go
+++ store/dispatch.go
@@ -14,15 +14,103 @@
 
 package store
 
+import (
+	"github.com/op/go-logging"
+	"github.com/projectcalico/libcalico/lib"
+)
+
+var log = logging.MustGetLogger("store")
+
 type Dispatcher struct {
+	OnEndpointUpdate       func(key *libcalico.EndpointKey, endpoint *libcalico.Endpoint)
+	OnEndpointDelete       func(key *libcalico.EndpointKey)
+	OnEndpointParseFailure func(key *libcalico.EndpointKey, err error)
+
+	OnHostEndpointUpdate       func(key *libcalico.HostEndpointKey, hostEndpoint *libcalico.HostEndpoint)
+	OnHostEndpointDelete       func(key *libcalico.HostEndpointKey)
+	OnHostEndpointParseFailure func(key *libcalico.HostEndpointKey, err error)
+
+	OnPolicyUpdate       func(key *libcalico.PolicyKey, endpoint *libcalico.Policy)
+	OnPolicyDelete       func(key *libcalico.PolicyKey)
+	OnPolicyParseFailure func(key *libcalico.PolicyKey, err error)
+
+	OnTierMetadataUpdate       func(key *libcalico.TierMetadataKey, endpoint *libcalico.TierMetadata)
+	OnTierMetadataDelete       func(key *libcalico.TierMetadataKey)
+	OnTierMetadataParseFailure func(key *libcalico.TierMetadataKey, err error)
 }
 
+// NewDispatcher creates a Dispatcher with all its event handlers set to no-ops.
 func NewDispatcher() *Dispatcher {
-	return &Dispatcher{}
+	d := Dispatcher{
+		OnEndpointUpdate:       func(key *libcalico.EndpointKey, endpoint *libcalico.Endpoint) {},
+		OnEndpointDelete:       func(key *libcalico.EndpointKey) {},
+		OnEndpointParseFailure: func(key *libcalico.EndpointKey, err error) {},
+
+		OnHostEndpointUpdate:       func(key *libcalico.HostEndpointKey, hostEndpoint *libcalico.HostEndpoint) {},
+		OnHostEndpointDelete:       func(key *libcalico.HostEndpointKey) {},
+		OnHostEndpointParseFailure: func(key *libcalico.HostEndpointKey, err error) {},
+
+		OnPolicyUpdate:       func(key *libcalico.PolicyKey, endpoint *libcalico.Policy) {},
+		OnPolicyDelete:       func(key *libcalico.PolicyKey) {},
+		OnPolicyParseFailure: func(key *libcalico.PolicyKey, err error) {},
+
+		OnTierMetadataUpdate:       func(key *libcalico.TierMetadataKey, endpoint *libcalico.TierMetadata) {},
+		OnTierMetadataDelete:       func(key *libcalico.TierMetadataKey) {},
+		OnTierMetadataParseFailure: func(key *libcalico.TierMetadataKey, err error) {},
+	}
+	return &d
 }
 
-type UpdateFunc func(update Update, captures ...string)
-
-func (dispatcher Dispatcher) RegisterPath(path string, onUpdate UpdateFunc) {
-
+func (d Dispatcher) DispatchUpdate(update Update) {
+	key := libcalico.ParseKey(update.Key)
+	if key == nil {
+		// Unknown key.
+		return
+	}
+	switch key := key.(type) {
+	case libcalico.EndpointKey:
+		if update.ValueOrNil == nil {
+			d.OnEndpointDelete(key)
+		} else {
+			endpoint, err := libcalico.ParseEndpoint(key, []byte(*update.ValueOrNil))
+			if err != nil {
+				d.OnEndpointParseFailure(key, err)
+			} else {
+				d.OnEndpointUpdate(key, endpoint)
+			}
+		}
+	case libcalico.HostEndpointKey:
+		if update.ValueOrNil == nil {
+			d.OnHostEndpointDelete(key)
+		} else {
+			hostEndpoint, err := libcalico.ParseHostEndpoint(key, []byte(*update.ValueOrNil))
+			if err != nil {
+				d.OnHostEndpointParseFailure(key, err)
+			} else {
+				d.OnHostEndpointUpdate(key, hostEndpoint)
+			}
+		}
+	case libcalico.PolicyKey:
+		if update.ValueOrNil == nil {
+			d.OnPolicyDelete(key)
+		} else {
+			policy, err := libcalico.ParsePolicy(key, []byte(*update.ValueOrNil))
+			if err != nil {
+				d.OnPolicyParseFailure(key, err)
+			} else {
+				d.OnPolicyUpdate(key, policy)
+			}
+		}
+	case libcalico.TierMetadataKey:
+		if update.ValueOrNil == nil {
+			d.OnTierMetadataDelete(key)
+		} else {
+			endpoint, err := libcalico.ParseTierMetadata(key, []byte(*update.ValueOrNil))
+			if err != nil {
+				d.OnTierMetadataParseFailure(key, err)
+			} else {
+				d.OnTierMetadataUpdate(key, endpoint)
+			}
+		}
+	}
 }
